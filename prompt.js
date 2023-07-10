03dd85479b0d8ea7c390f22afd442db482f61ee3
Anshu <singhanshu756@gmail.com>
2023-07-10 10:47:42 +0530

    this is 2nd javascript program

---
+++ prompt.js
@@ -0,0 +1,7 @@
+//sum of two number and taking no by users
+const prompt = require("prompt-sync")();
+
+var a = prompt("enter the value of a: ");
+var b = prompt("enter the value of b: ");
+var sum = Number(a)+Number(b);
+console.log("sum of two no: "+ sum);
