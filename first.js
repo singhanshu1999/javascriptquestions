c31f51e6c21eac40afe20a084edf77bcdc3d7bd1
Anshu <singhanshu756@gmail.com>
2023-07-10 10:36:34 +0530

    adding first js program

---
+++ first.js
@@ -0,0 +1,10 @@
+
+var firstName = "Anshu";
+var lastName = "Kumar";
+console.log(firstName+" "+lastName);
+
+//sum of two number
+var a = 23;
+var b = 43;
+console.log("sum of two number: "+(a+b));
+
